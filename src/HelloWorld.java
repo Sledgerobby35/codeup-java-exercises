afd07d0e5414c8709ac16a97f1e45b127d0706d5
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-21 20:03:54 -0400

    add Hello World java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,42 @@
 public class HelloWorld {
     public static void main(String[] args){
         //ALL JAVA STRINGS MUST BE IN DOUBLE QUOTES
-        System.out.println("Hello, world");
+        int myFavoriteNumber = 2614;
+        System.out.println(myFavoriteNumber);
+
+//        String myString = "Hello, World!!";
+//        System.out.println(myString);
+
+//        char myString = 'R';
+//        System.out.println(myString);
+
+//        String myString = 3.14159;
+//        System.out.println(myString);
+
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
+
+//        Object o = "three";
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        x *= y;
+//        System.out.println(x);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+
     }
 }
