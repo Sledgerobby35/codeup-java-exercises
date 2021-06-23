33fe15393e1650bdd35b401cdb0a55feed1f01b8
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-23 16:22:41 -0400

    add class notes

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -47,19 +47,105 @@ public class ConsoleIO {
 //
 //        String myString = sc.nextLine();
 //        System.out.println(myString);
-        boolean hasNL = sc.hasNext();
-
-        while (hasNL){
-            String word = sc.next();
-
-            if(word.equals("break")){
-                break;
-            }
-            
-            System.out.println(word);
-        }
+//        boolean hasNL = sc.hasNext();
+//
+//        while (hasNL){
+//            String word = sc.next();
+//
+//            if(word.equals("break")){
+//                break;
+//            }
+//
+//            System.out.println(word);
+//        }
 //        int myNum = sc.nextInt();
 //        System.out.println(myNum);
+
+//        String myName = "Robby";
+//        String yourName = "robby";
+////              .equals()
+//        System.out.println(myName.equals(yourName));
+//        //      .equalsIgnoreCase()
+//        System.out.println(myName.equalsIgnoreCase(yourName));
+
+        //        MINI EXERCISE
+        // TODO: create three unique usernames (user1, user2, user3).
+        //  create a fourth user and set it equal to user input. (ask
+        //  the user to enter a username). if the username has already
+        //  been used by user1, user2, or user3, print "sorry this
+        //  username already exists.
+
+//        String user1 = "Relort35";
+//        String user2 = "Amunzetsu";
+//        String user3 = "SledgeHammer";
+//
+//        System.out.println("Please input a username");
+//        String user4 = sc.next();
+//        if(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3)){
+//            System.out.println("Sorry this username already exists");
+//        } else {
+//            System.out.printf("The username you entered is: %s", user4);
+//        }
+//        String takenUserName = "Sorry this username is already registered";
+//
+//        switch(user4){
+//            case "Relort35":
+//                System.out.println(takenUserName);
+//                break;
+//            case "Amunzetsu":
+//                System.out.println(takenUserName);
+//                break;
+//            case "SledgeHammer":
+//                System.out.println(takenUserName);
+//                break;
+//            default:
+//                System.out.printf("The username you wish to register is: %s", user4);
+//        }
+
+        //while vs do...while loop:
+        //condition is checked before
+        //do ... while will always be executed first before condition is checked
+
+//        int num1 = 0;
+
+//        while(num1 <= 10){
+//            System.out.println(num1);
+//            num1++;
+//        }
+
+//        do {
+//            System.out.println(num1);
+//            num1++;
+//        } while(num1 <= 10);
+
+
+//        for... loop
+
+//        int myNum = 18;
+
+//        for(int myNum = 0; myNum <= 20; myNum++){
+//            System.out.println(myNum);
+//        }
+        String user1 = "Robby";
+        String user2 = "Tiffany";
+        String user3 = "Relort35";
+//        String user4 = sc.next();
+
+//        if(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3)){
+//            System.out.println("Sorry this username already exists");
+//        } else {
+//            System.out.printf("The username you entered is: %s", user4);
+//        }
+
+
+//        while(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3)){
+//            System.out.println("Sorry this username already exists");
+//        }
+        String user4 = sc.next();
+        do{
+            System.out.println("Please input a username");
+            sc.next();
+        } while(user4.equalsIgnoreCase(user1) || user4.equalsIgnoreCase(user2) || user4.equalsIgnoreCase(user3));
     }
 }
 
