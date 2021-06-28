bd359c0fe60dbc004f0d4fc8e1f60be07a62c0f9
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-28 16:52:23 -0500

    add java class from exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String example1 = "We don't need no education\n";
+        System.out.println(example1);
+
+        String example2 = "We don't need no thought control\n";
+        System.out.println(example2);
+
+        String example3 = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(example3);
+
+        String example4 = "In windows, the main drive is usually C:\\ \n";
+        System.out.println(example4);
+
+        String example5 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(example5);
+    }
+}
