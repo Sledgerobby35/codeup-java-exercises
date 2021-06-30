9da0c65279e595f167eddff6293aa1a44c7bba4c
Sledgerobby35 <sledge.robby35@gmail.com>
2021-06-30 18:42:20 -0500

    add exercises for methods

---
+++ src/MethodExercises.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class MethodExercises {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input a number between 1 and 10");
+        int userInput = sc.nextInt();
+        getInteger(1,10, sc);
+        factorial(userInput);
+
+    }
+    public static float add(float num1, float num2){
+        return num1 + num2;
+    }
+    public static float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+    public static float multiply(float num1, float num2){
+        return num1 * num2;
+    }
+    public static float divide(float num1, float num2){
+        if(num2 == 0){
+            return 0;
+        } else {
+            return num1 / num2;
+        }
+    }
+    public static float modulus(float num1, float num2){
+        return num1 % num2;
+    }
+
+
+    public static int getInteger(int min, int max, Scanner sc){
+        sc.next();
+        int userInput = 0;
+        System.out.printf("Please input a number between %s and %s\n",1, 10);
+        if(userInput <= min || userInput >= max){
+            getInteger(min, max, sc);
+        }
+        return userInput;
+    }
+//    public static int getInteger(int min, int max,int userInput){
+//        if(userInput >= min && userInput <= max){
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+//    }
+//    public static boolean getIntLogic(int userInput){
+//        Scanner sc = new Scanner(System.in);
+//        if(getInteger(1,10,userInput) == 0){
+//            System.out.println("Please input a number between 1 and 10");
+//            getIntLogic(sc.nextInt());
+//            return false;
+//        } else{
+//            System.out.println("true");
+//            return true;
+//        }
+//    }
+public static void factorial(int userInput){
+    int x = 1;
+    String factorial = "";
+        for(int i = userInput; i > 0; i--){
+            x *= i;
+            factorial += (i + " * ");
+            if(proceed()){
+                System.out.println(factorial);
+                System.out.println(x);
+                proceed();
+            } else {
+                System.out.println(x);
+            }
+        }
+}
+public static boolean proceed(){
+        Scanner sc = new Scanner(System.in);
+        String userConfirmation = sc.next();
+    System.out.println("Would you like to continue?\n Yes or No");
+    return userConfirmation.equalsIgnoreCase("Yes");
+}
+}
