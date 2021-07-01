454b6c104b7c834a6cb67b3db4e9974a35eaf16f
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-01 17:32:11 -0500

    add changes made in class

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MethodExercises {
@@ -5,37 +6,39 @@ public class MethodExercises {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input a number between 1 and 10");
         int userInput = sc.nextInt();
-        getInteger(1,10, sc);
+//        System.out.println(getInteger(1,10, sc));
         factorial(userInput);
+//        Oberon classInfo = new Oberon();
+//        System.out.printf("There are %s students in class, we are learning %s, and we" +
+//                " graduate in %s days on October 1st",
+//                classInfo.numOfStudents, Arrays.toString(classInfo.topicsOfLearning), classInfo.dayUntilGraduation);
 
     }
-    public static float add(float num1, float num2){
-        return num1 + num2;
-    }
-    public static float subtract(float num1, float num2){
-        return num1 - num2;
-    }
-    public static float multiply(float num1, float num2){
-        return num1 * num2;
-    }
-    public static float divide(float num1, float num2){
-        if(num2 == 0){
-            return 0;
-        } else {
-            return num1 / num2;
-        }
-    }
-    public static float modulus(float num1, float num2){
-        return num1 % num2;
-    }
+//    public static float add(float num1, float num2){
+//        return num1 + num2;
+//    }
+//    public static float subtract(float num1, float num2){
+//        return num1 - num2;
+//    }
+//    public static float multiply(float num1, float num2){
+//        return num1 * num2;
+//    }
+//    public static float divide(float num1, float num2){
+//        if(num2 == 0){
+//            return 0;
+//        } else {
+//            return num1 / num2;
+//        }
+//    }
+//    public static float modulus(float num1, float num2){
+//        return num1 % num2;
+//    }
 
 
-    public static int getInteger(int min, int max, Scanner sc){
-        sc.next();
-        int userInput = 0;
-        System.out.printf("Please input a number between %s and %s\n",1, 10);
+    public static int getInteger(int min, int max, int userInput){
         if(userInput <= min || userInput >= max){
-            getInteger(min, max, sc);
+            System.out.printf("Please input a number between %s and %s\n",1, 10);
+//            getInteger(min, max, userInput);
         }
         return userInput;
     }
