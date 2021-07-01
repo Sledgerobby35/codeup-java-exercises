9742ce7ae164952ff6094f6c11a1bd40e4da8c42
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-01 17:32:11 -0500

    add exercise from class

---
+++ src/Oberon.java
@@ -0,0 +1,29 @@
+public class Oberon {
+    // TODO: CREATE A NEW "OBERON" CLASS WITH AT LEAST 6 FIELDS
+    //  THAT DESCRIBE OUR OBERON COHORT. MAKE SURE TO INCLUDE
+    //  AT LEAST THREE DIFFERENT TYPES OF DATA IN YOUR CLASS. IN A
+    //  SEPARATE FILE, CREATE 3 INSTANCES OF THE OBERON CLASS AND
+    //  PRINT OUT A STRING THAT USES AT LEAST 3 OF THOSE FIELDS
+    //  TO DESCRIBE THAT OBERON INSTANCE.
+    public static void main(String[] args) {
+
+    }
+
+    int numOfStudents = 24;
+    boolean softwareDevelopers = true;
+    String[] studentsArray = {
+            "Amado", "Christian", "Christopher", "Corey", "Demetrio",
+            "Diamond", "Erik", "Grady", "Jesse", "Joshua",
+            "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
+            "Raul", "Ricardo", "Richard", "Robert", "Sam",
+            "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
+            "Casey", "Dorian", "Tristan", "Jordy"
+    };
+    String[] topicsOfLearning = {
+            "HTML, CSS, Bootstrap,JavaScript, jQuery, Java SE, Java EE (Servlets, \n" +
+                    "JSP),  Spring, Thymeleaf, MySQL, Object-Oriented Programming,  " +
+                    "Test-Driven Development, Version  Control with Git, Paired Programming\n"
+    };
+    int dayUntilGraduation = 67;
+
+}
