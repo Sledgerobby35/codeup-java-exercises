dbaa1a87c1595e6299481a40d9ebac5bca7b1026
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-01 17:32:11 -0500

    add circle exercise

---
+++ src/shapes/Circle.java
@@ -0,0 +1,25 @@
+package shapes;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(double radius){
+        return (Math.PI * (radius * radius));
+    }
+    public double getCircumference(double radius){
+        return (2 * Math.PI * radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
