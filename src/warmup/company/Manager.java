6cfc7c8a540d69136dec64aaebf9ff7e6481fce3
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-06 23:30:14 -0500

    add class notes on interface Manager file

---
+++ src/warmup/company/Manager.java
@@ -0,0 +1,19 @@
+package warmup.company;
+
+public class Manager extends Employee{
+    public Manager(String salary, String name, String department){
+        super(salary,name, department);
+    }
+
+    public static void main(String[] args) {
+//        Employee Jerry = new Manager("10K","Jerry", "Marketing");
+//        System.out.println(Jerry.info());
+//        System.out.println(Jerry.doWork());
+//
+//        Employee Bob = new Manager("15K", "Bob", "Sales");
+//        System.out.println(Bob.info());
+//        System.out.println(Bob.doWork());
+        int x = 5 * 4 % 3;
+        System.out.println(x);
+    }
+}
