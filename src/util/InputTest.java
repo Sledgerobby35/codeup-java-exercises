c57a93302b062fd5d42f9e6bf108fae03862b9d1
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-12 15:24:50 -0500

    add InputTest Class from OOP exercises

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,2 +1,25 @@
-package util;public class InputTest {
+package util;
+
+import static util.Input.*;
+
+public class InputTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(getString());
+
+        System.out.println("Is today well?");
+        System.out.println(yesNo());
+
+        System.out.println("Please input a number between 1 and 10");
+        System.out.println(getInt(1, 10));
+
+        System.out.println("Please input a number");
+        System.out.println(getInt());
+
+        System.out.println("Please input a decimal between 0.5 and 10.5");
+        System.out.println(getDouble(0.5, 10.5));
+
+        System.out.println("Please input a decimal number");
+        System.out.println(getDouble());
+    }
 }
