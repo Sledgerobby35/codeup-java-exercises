4692db31e03c7cae5a7a52b445e5f6151c5fa32f
Sledgerobby35 <sledge.robby35@gmail.com>
2021-07-15 23:40:00 -0500

    add FileIO logic to fileIO directory

---
+++ src/fileIO/FileDirectoryUtil.java
@@ -0,0 +1,53 @@
+package fileIO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDirectoryUtil {
+    public static Path getPath(String fileName){
+        return Paths.get(fileName);
+    }
+
+    public static Path getPath(String parentDirectory, String fileName){
+        return Paths.get(parentDirectory, fileName);
+    }
+
+    public static Path getPath(String parentDirectory, String childDirectory,
+                               String fileName){
+        return Paths.get(parentDirectory, childDirectory, fileName);
+    }
+
+    public static boolean doesPathExist(Path path){
+        return Files.exists(path);
+    }
+
+    public static void tryCreateDirectory(Path pathToCreate){
+        if(!doesPathExist(pathToCreate)){
+            try{
+                Files.createDirectory(pathToCreate);
+            } catch(IOException e) {
+                System.out.println("Could not create the directory at: ");
+                System.out.println(pathToCreate.toAbsolutePath());
+            }
+        } else {
+            System.out.println("The path at: " + pathToCreate.toAbsolutePath() +
+                    " already exists");
+        }
+    }
+
+    public static void tryCreateFile(Path path){
+        if(!doesPathExist(path)){
+            try{
+                Files.createFile(path);
+            }catch(IOException e){
+                System.out.println("Could not create the file at: ");
+                System.out.println(path.toAbsolutePath());
+            }
+        } else {
+            System.out.println("The file at " + path.toAbsolutePath() + " already " +
+                    "exists");
+        }
+    }
+}
